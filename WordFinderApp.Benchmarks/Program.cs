23d4824b7a216145de80fb7568d5fc803d2d4655
netoo89 <flanagas2@hotmail.com>
2024-12-06 10:00:29 -0800

    Include benchmark project to check word matchers

---
+++ WordFinderApp.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace WordFinderApp.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MatchersBenchmark>();
+        }
+    }
+}
---
+++ WordFinderApp/Extensions/EnumerableCharExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFinderApp.Extensions
+{
+    public static class EnumerableCharExtensions
+    {
+        public static string CastToString(this IEnumerable<char> chars)
+        {
+            return string.Concat(chars);
+        }
+    }
+}
